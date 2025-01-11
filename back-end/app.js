55653f21c24f90f4870958b2a227dadb23ea6722
shatakshi <sshatakshi841@gmail.com>
2025-01-11 21:02:03 +0530

    initial

---
+++ back-end/app.js
@@ -0,0 +1,18 @@
+const express= require('express');
+const app=express();
+const mongoose=require('mongoose');
+const cors=require('cors')
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/Quote')
+.then(()=>{console.log('Db connected successfully')})
+.catch((err)=>{console.log(err)})
+ 
+app.use(cors({origin:['http://localhost:3000']}));
+
+const PORT = process.env.PORT
+
+const port=process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log(`server connected at port ${port}`)
+})
